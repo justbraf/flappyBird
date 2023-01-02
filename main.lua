79044302f6fa3b3d2bb79182e2b6a6541a33186b
justbraf <dwaynecorey@gmail.com>
2023-01-02 18:49:14 -0400

    Parallax effect

--- main.lua
+++ main.lua
@@ -6,18 +6,29 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+local bkgnd = love.graphics.newImage('assets/background.png')
+local bkScroll = 0
+
+local ground = love.graphics.newImage('assets/ground.png')
+local gndScroll = 0
+
 function love.load()
     math.randomseed(os.time())
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    love.window.setTitle("Flappy Bird")
+    love.window.setTitle("Fifty Bird")
 
-    bkgnd = love.graphics.newImage('assets/background.png')
+    medFont = love.graphics.newFont('assets/font.ttf', 16)
+    love.graphics.setFont(medFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.keypressed(key)
@@ -26,6 +37,18 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    bkScroll = bkScroll - 50 * dt
+    gndScroll = gndScroll - 100 * dt
+    if gndScroll < -1050 + VIRTUAL_WIDTH then
+        gndScroll = 0
+    end
+end
+
 function love.draw()
-    love.graphics.draw(bkgnd, 100, 100)    
+    push:start()
+    love.graphics.draw(bkgnd, bkScroll, 0)
+    love.graphics.draw(ground, gndScroll, VIRTUAL_HEIGHT - 16)
+    love.graphics.print(gndScroll, 10, 10)
+    push:finish()
 end
