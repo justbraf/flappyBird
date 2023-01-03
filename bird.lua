8e0ac94927e779aff73cc695970b6585f9e3ff16
justbraf <dwaynecorey@gmail.com>
2023-01-02 21:19:11 -0400

    bird implementation

---
+++ bird.lua
@@ -0,0 +1,31 @@
+Bird = Class {}
+
+local GRAVITY = 20
+
+function Bird:init()
+    self.image = love.graphics.newImage('assets/bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+    self.x = VIRTUAL_WIDTH / 2 - self.width / 2
+    self.y = VIRTUAL_HEIGHT / 2 - self.height / 2
+    self.dy = 0
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+    self.y = self.y + self.dy
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
+end
+
+function Bird:collides()
+    if self.y > VIRTUAL_HEIGHT - 16 - self.height then
+        return true
+    end
+    return false
+end
--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 push = require 'push'
+Class = require 'class'
+
+require 'bird'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,11 +11,11 @@ VIRTUAL_HEIGHT = 288
 
 local bkgnd = love.graphics.newImage('assets/background.png')
 local bkScroll = 0
-local bkScrollSpeed = 50
+local BACKGROUND_SCROLL_SPEED = 30
 
 local ground = love.graphics.newImage('assets/ground.png')
 local gndScroll = 0
-local gndScrollSpeed = 100
+local GROUND_SCROLL_SPEED = 60
 
 function love.load()
     math.randomseed(os.time())
@@ -27,6 +30,9 @@ function love.load()
         resizable = true,
         vsync = true
     })
+
+    bird = Bird()
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -34,27 +40,48 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
     if key == 'escape' then
         love.event.quit()
     end
 end
 
-function love.update(dt)
-    bkScroll = bkScroll - 50 * dt
-    if bkScroll < -413 then
-        bkScroll = 0
-    end
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
 
-    gndScroll = gndScroll - 100 * dt
-    if gndScroll < -1100 + VIRTUAL_WIDTH then
-        gndScroll = 0
+function love.update(dt)
+    if not bird:collides() then
+        bkScroll = bkScroll - BACKGROUND_SCROLL_SPEED * dt
+        if bkScroll < -413 then
+            bkScroll = 0
+        end
+
+        gndScroll = gndScroll - GROUND_SCROLL_SPEED * dt
+        if gndScroll < -1100 + VIRTUAL_WIDTH then
+            gndScroll = 0
+        end
+
+        if love.keyboard.isDown('up') then
+            bird.y = bird.y - 10
+            if bird.y < 0 then
+                bird.y = 0
+            end
+        end
+        bird:update(dt)
+        love.keyboard.keysPressed = {}
     end
 end
 
 function love.draw()
     push:start()
+
     love.graphics.draw(bkgnd, bkScroll, 0)
-    love.graphics.draw(ground, gndScroll, VIRTUAL_HEIGHT - 16)
-    love.graphics.print(gndScroll, 10, 10)
+    love.graphics.draw(ground, gndScroll, VIRTUAL_HEIGHT - ground:getHeight())
+    -- love.graphics.print(love.getVersion() .. gndScroll, 10, 10)
+
+    bird:render()
+
     push:finish()
 end
