a7acc09187b1f82c8d0c08eba9b4aca5939cdad6
justbraf <dwaynecorey@gmail.com>
2023-01-22 10:47:33 -0400

    update bird y limits

--- bird.lua
+++ bird.lua
@@ -16,25 +16,30 @@ function Bird:render()
 end
 
 function Bird:update(dt)
-    self.dy = self.dy + GRAVITY * dt
-    self.y = self.y + self.dy
     if love.keyboard.wasPressed('space') then
         self.dy = -5
+    end
+    self.dy = self.dy + GRAVITY * dt
+    self.y = self.y + self.dy
+    -- if the bird sprite is above the view port then limit its y position
+    if self.y < 0 then
+        self.y = 0
     end
 end
 
 function Bird:collides(pipePair)
+    -- if the bird has flown pass a pipePair then ignore them
     if self.x + 2 > pipePair.pipes['upper'].x + pipePair.width then
         return false
     end
-    if self.x + self.width - 2 > pipePair.pipes['upper'].x and self.y + 2 < pipePair.pipes['upper'].y then
-        print('upperpipe.x ' .. pipePair.pipes['upper'].y)
-        return true
-    elseif self.x + self.width - 2 > pipePair.pipes['upper'].x and self.y + self.height - 2 > pipePair.pipes['lower'].y then
-        print('lowerpipe.x ' .. pipePair.pipes['lower'].y)
+    -- if the bird is not within the pipe gap then indicate a collision
+    if self.x + self.width - 2 > pipePair.pipes['upper'].x and
+        (self.y + 2 < pipePair.pipes['upper'].y or self.y + self.height - 2 > pipePair.pipes['lower'].y) then
+        print('pipe.x ' .. pipePair.pipes['lower'].y)
         return true
     elseif self.y > VIRTUAL_HEIGHT - 16 - self.height - 2 then
         return true
     end
+    -- no conditions have been met. The bird is flying in open space
     return false
 end
